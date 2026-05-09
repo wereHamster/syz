8e8eca5a6023524bdeb9829f1ada960b3b3e572f
Tomas Carnecky <tomc@caurea.org>
2026-05-09 16:33:59 +0200

    Init syz

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
---
+++ src/bin/syzd.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use tracing_subscriber::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    init_tracing_subscriber();
+
+    tracing::info!("syzd");
+
+    Ok(())
+}
+
+pub fn init_tracing_subscriber() {
+    let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info"));
+
+    // If we detect that stdout/stderr is connected to journald, use the
+    // journald-specific layer.
+    //
+    // If connecting to journald fails, fall through to the fmt subscriber.
+    if std::env::var("JOURNAL_STREAM").is_ok() {
+        if let Ok(journald_layer) = tracing_journald::layer() {
+            tracing_subscriber::registry()
+                .with(env_filter)
+                .with(journald_layer)
+                .init();
+            return;
+        }
+    }
+
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
+}
