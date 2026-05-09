8cf0d2efc4e037858171126089a7e8b8f05a3844
Tomas Carnecky <tomc@caurea.org>
2026-05-09 18:53:21 +0200

    Outline implementation of AnalyzeProjectDependencies

--- src/core/actions/analyze_project_dependencies.rs
+++ src/core/actions/analyze_project_dependencies.rs
@@ -11,5 +11,18 @@ pub async fn run(app: &Application, project_id: String) -> Result<()> {
 
     tracing::info!("Got project {}:{}", project.platform, project.repository);
 
+    // project.platform is either "github" or "tangled".
+    //
+    // Need to construct a ProjectRepositoryView, get the revision of the default branch,
+    // then construct a ProjectRepositorySnapshot.
+    //
+    // Then initialize ecosystem scanners and apply each to the snapshot to discover
+    // project dependencies. Run the discovery in parallel.
+    //
+    // Once all project dependencies are known, query the ecosystem for dependency update
+    // options.
+    //
+    // Finally write the result into the database.
+
     Ok(())
 }
